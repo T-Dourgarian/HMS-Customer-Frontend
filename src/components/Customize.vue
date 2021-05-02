51228fb477a5ed591774655e81ad79fed867cc9c
T-Dourgarian <thomasdourgarian@gmail.com>
2021-05-02 14:36:17 -0500

    stepper for booking process

---
+++ src/components/Customize.vue
@@ -0,0 +1,89 @@
+<template>
+	<v-row class="ma-0">
+		<v-col>
+			<v-row class="ma-0" style="backgroundColor: rgb(240, 240, 240);">
+				<v-col>
+					<v-card  class="listingContainer" elevation="1">
+						<v-row class="pa-0 ma-0">
+							<v-col cols="4" class="pa-0">
+							<b-carousel
+								:pause-hover="false"
+								:autoplay="false"
+								style="height:250px"
+							>
+								<b-carousel-item v-for="(image, i) in listing.images" :key="i" >
+									
+										
+									<img style="height:250px; width:100%" :src="'http://localhost:3000' + image.path" alt="">
+										
+									
+								</b-carousel-item>
+							</b-carousel>
+						</v-col>
+						<v-col cols="8" style="position: relative;">
+							<v-row>
+								<v-col class="pb-0">
+									<v-row class="ma-0">
+										<v-col cols="3" class=" pl-0 pt-0">
+											<div class="roomTitle">
+												{{ listing.name }}
+											</div>
+
+											<div class="roomSubtitle">
+												{{ listing.subtitle }}
+											</div>
+										</v-col>
+										<v-col class="pr-0 pt-0">
+											<!-- <v-row class="ma-0 pa-0">
+												<v-col cols="3" class="amenityContainer" v-for="(amenityPair, i) in formatAmenityArray(listing.amenities)" :key="i">
+													<div class="amenity" v-for="(amenity, i) in amenityPair" :key="i">
+														<v-icon x-small color="rgb(131, 114, 84)">fas fa-{{ amenity.icon }}</v-icon>
+														<span> {{ amenity.name }} </span>
+													</div>
+												</v-col>
+											</v-row> -->
+										</v-col>
+									</v-row>
+								</v-col>
+								<v-col class="pt-0" cols="12">
+									<div class="roomDescription">
+										{{ listing.description }}
+									</div>
+								</v-col>
+							</v-row>
+							<template>
+								<button class="bookButton"  @click.prevent="bookListing(listing)">
+									Book
+									<v-icon color="#e0e0e0" small> fas fa-chevron-right</v-icon> 
+								</button>
+							</template>
+
+							<div class="price">
+								${{ listing.basePrice }} <span class="perNight"> / Night </span>
+							</div>
+						</v-col>
+						</v-row>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			
+		}
+	},
+	props:[ 'listing' ],
+	created() {
+		console.log(this.listing)
+	}
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Listings.vue
+++ src/components/Listings.vue
@@ -1,7 +1,7 @@
 <template>
 	<div v-if="listings">
 		
-		<v-row style="margin: 0!important" class="dateSearchRow" justify="center" align="center">
+		<v-row  class="dateSearchRow ma-0" justify="center" align="center">
 			<v-col cols="3">
 				<div class="dateLabel">Arrival</div>
 				<b-datepicker
@@ -40,69 +40,91 @@
 				{{ listings[0].numberOfNights }} Nights, starting at ${{ listings[0].totalPrice }}
 			</div> -->
 
-		<v-row class="ma-0" style="backgroundColor: rgb(240, 240, 240);">
-			<v-col>
-				<v-card v-for="listing in listings" :key="listing.uuid" class="listingContainer" elevation="1">
-					<v-row class="pa-0 ma-0">
-						<v-col cols="4" class="pa-0">
-						<b-carousel
-							:pause-hover="false"
-							:autoplay="false"
-							style="height:250px"
-						>
-							<b-carousel-item v-for="(image, i) in listing.images" :key="i" >
-								
-									
-								<img style="height:250px; width:100%" :src="'http://localhost:3000' + image.path" alt="">
-									
-								
-							</b-carousel-item>
-						</b-carousel>
-					</v-col>
-					<v-col cols="8" style="position: relative;">
-						<v-row>
-							<v-col class="pb-0">
-								<v-row class="ma-0">
-									<v-col cols="3" class=" pl-0 pt-0">
-										<div class="roomTitle">
-											{{ listing.name }}
-										</div>
 
-										<div class="roomSubtitle">
-											{{ listing.subtitle }}
-										</div>
-									</v-col>
-									<v-col class="pr-0">
-										<v-row class="ma-0 pa-0">
-											<v-col cols="3" class="amenityContainer" v-for="(amenityPair, i) in formatAmenityArray(listing.amenities)" :key="i">
-												<div class="amenity" v-for="(amenity, i) in amenityPair" :key="i">
-													<v-icon x-small color="rgb(131, 114, 84)">fas fa-{{ amenity.icon }}</v-icon>
-													<span> {{ amenity.name }} </span>
+		<b-steps
+            v-model="activeStep"
+            :animated="true"
+            :rounded="true"
+            :has-navigation="false"
+			class="steps"
+		>
+            <b-step-item icon="fas fa-bed" label="Select a room" :clickable="activeStep > 0 ? true: false" >
+				<v-row class="ma-0" style="backgroundColor: rgb(240, 240, 240);">
+					<v-col>
+						<v-card v-for="listing in listings" :key="listing.uuid" class="listingContainer" elevation="1">
+							<v-row class="pa-0 ma-0">
+								<v-col cols="4" class="pa-0">
+								<b-carousel
+									:pause-hover="false"
+									:autoplay="false"
+									style="height:250px"
+								>
+									<b-carousel-item v-for="(image, i) in listing.images" :key="i" >
+										
+											
+										<img style="height:250px; width:100%" :src="'http://localhost:3000' + image.path" alt="">
+											
+										
+									</b-carousel-item>
+								</b-carousel>
+							</v-col>
+							<v-col cols="8" style="position: relative;">
+								<v-row>
+									<v-col class="pb-0">
+										<v-row class="ma-0">
+											<v-col cols="3" class=" pl-0 pt-0">
+												<div class="roomTitle">
+													{{ listing.name }}
 												</div>
+
+												<div class="roomSubtitle">
+													{{ listing.subtitle }}
+												</div>
+											</v-col>
+											<v-col class="pr-0 pt-0">
+												<v-row class="ma-0 pa-0">
+													<v-col cols="3" class="amenityContainer" v-for="(amenityPair, i) in formatAmenityArray(listing.amenities)" :key="i">
+														<div class="amenity" v-for="(amenity, i) in amenityPair" :key="i">
+															<v-icon x-small color="rgb(131, 114, 84)">fas fa-{{ amenity.icon }}</v-icon>
+															<span> {{ amenity.name }} </span>
+														</div>
+													</v-col>
+												</v-row>
 											</v-col>
 										</v-row>
 									</v-col>
+									<v-col class="pt-0" cols="12">
+										<div class="roomDescription">
+											{{ listing.description }}
+										</div>
+									</v-col>
 								</v-row>
-							</v-col>
-							<v-col class="pt-0" cols="12">
-								<div class="roomDescription">
-									{{ listing.description }}
+								<template>
+									<button class="bookButton"  @click.prevent="bookListing(listing)">
+										Book
+										<v-icon color="#e0e0e0" small> fas fa-chevron-right</v-icon> 
+									</button>
+								</template>
+
+								<div class="price">
+									${{ listing.basePrice }} <span class="perNight"> / Night </span>
 								</div>
 							</v-col>
-						</v-row>
-						<button class="bookButton">
-							Book
-							<v-icon color="#e0e0e0" small> fas fa-chevron-right</v-icon> 
-						</button>
-
-						<div class="price">
-							${{ listing.basePrice }} <span class="perNight"> / Night </span>
-						</div>
+							</v-row>
+						</v-card>
 					</v-col>
-					</v-row>
-				</v-card>
-			</v-col>
-		</v-row>
+				</v-row>
+            </b-step-item>
+
+            <b-step-item icon="fas fa-clipboard-list" label="Booking Details"   >
+                <Customize :listing="listingToBook" />
+            </b-step-item>
+
+            <b-step-item icon="fas fa-check" label="Confirm Booking">
+                Lorem ipsum dolor sit amet.
+            </b-step-item>
+
+        </b-steps>
 
 
 
@@ -156,6 +178,7 @@
 import axios from 'axios';
 import BookingDialog from './BookingDialog';
 import moment from 'moment';
+import Customize from './Customize';
 
 export default {
 	name:'Listings',
@@ -168,9 +191,10 @@ export default {
 			minArrival: moment().subtract(1,'day').toDate(),
 			minDeparture: moment().toDate(),
 			searchLoading: false,
+			activeStep: 0
 		}
 	},
-	components: { BookingDialog },
+	components: { BookingDialog, Customize },
 	methods: {
 		async getListings() {
 			try {
@@ -210,9 +234,8 @@ export default {
 			return room.basePrice * diffDays;
 		},
 		async bookListing(listing) {
-
+			this.activeStep = 1
 			this.listingToBook = listing;
-			this.bookingDialog = true;
 		},
 		cancelBooking() {
 			this.bookingDialog = false;
@@ -281,6 +304,7 @@ export default {
 	font-weight: 300;
 	letter-spacing: 2px;
 	font-size: 19px;
+	color:#64563e;
 }
 
 .dateSearchRow {
@@ -300,32 +324,35 @@ export default {
 }
 
 .searchButton:hover {
-	background-color: rgb(151, 151, 151);
+	background-color: #a08e6e;
 }
 
 .listingContainer {
 	margin: 20px !important;
-	background-color: rgb(240, 240, 240);
+	/* background-color: rgb(240, 240, 240); */
 	height:250px;
+	border-radius: 0px !important;
 }
 
 .roomTitle {
-	color: #837254;
+	color: #837154;
 	font-family: 'Libre Baskerville', serif;
-	font-size: 25px;
+	font-weight: 600;
+	font-size: 20px;
 	letter-spacing: 1px;
 }
 
 .roomSubtitle {
 	font-family: 'Libre Baskerville', serif;
 	color: #837254;
-	font-size: 18px;
+	font-size: 16px;
 }
 
 
 .roomDescription {
 	font-family: 'Open Sans', sans-serif;
 	color: rgb(109, 109, 109);
+	font-size: 12px;
 }
 
 .amenityContainer {
@@ -358,8 +385,7 @@ export default {
 
 .price {
 	position:absolute;
-	font-family: 'Libre Baskerville', serif;
-	font-style: italic;
+	font-family: 'Open Sans', sans-serif;
 	color: #837254;
 	font-size: 20px;
 	bottom: 0;
@@ -370,4 +396,15 @@ export default {
 	font-size: 16px;
 }
 
+.step-content {
+	padding: 0 !important;
+	height:100vh;
+	overflow-y: scroll;
+}
+
+
+.steps {
+	background-color: rgb(240, 240, 240);
+	padding: 10px 0 0 0;
+}
 </style>
